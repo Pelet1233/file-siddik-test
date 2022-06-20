809d19bc33627c36ab2254f04cf5b34c562573c3
khairuzadi <okezobe11@gmail.com>
2022-06-20 16:13:38 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>testingi saja bro lagi2 tambah</title>
+	<title>testingi saja bro lagi2 tambah ntah kenapa ya</title>
 </head>
 <body>
 
